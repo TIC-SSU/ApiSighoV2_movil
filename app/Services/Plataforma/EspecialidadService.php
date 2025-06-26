807d7139177f900ea115f7fb408bfe228db09589
Eleazarguitar24 <Eleazarguitar24@gmail.com>
2025-06-26 09:00:30 -0400

    verificacion de edad 2

--- app/Services/Plataforma/EspecialidadService.php
+++ app/Services/Plataforma/EspecialidadService.php
@@ -58,7 +58,7 @@ class EspecialidadService
     }
     public function listar_especialidades($fecha, $id_persona_titular, $sexo, $fecha_nacimiento)
     {
-        $edad = Carbon::parse($fecha_nacimiento)->age;
+        $edad = Carbon::createFromFormat('d/m/Y', $fecha_nacimiento)->age;
         // dd($fecha, $id_persona_titular);
         /*$response = $this->agendaService->existeGrupoFamiliarAgendado($fecha, $id_persona_titular);
         // dd($response);
