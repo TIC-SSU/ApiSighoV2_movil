ef41c4a4c2b6330ed36c15db8b361ac035f4f6e0
Eleazarguitar24 <Eleazarguitar24@gmail.com>
2025-06-26 15:24:42 -0400

    probando porcentajes 9

--- app/Services/Plataforma/EspecialistaService.php
+++ app/Services/Plataforma/EspecialistaService.php
@@ -569,8 +569,9 @@ class EspecialistaService
 
         $porcentaje = 0;
 
+        dd($cantidadHorarios);
+
         foreach ($cantidadHorarios as $key => $cantidadHorario) {
-            dd(Carbon::now()->gte(Carbon::parse($cantidadHorario->hora_inicio)) && (Carbon::now()->lte(Carbon::parse($cantidadHorario->hora_fin))));
             $horasDisponibles[] = Carbon::parse($cantidadHorario->hora_inicio)->format('H:i') . ' - ' . Carbon::parse($cantidadHorario->hora_fin)->format('H:i');
             if ((Carbon::now()->gte(Carbon::parse($cantidadHorario->hora_inicio))) && (Carbon::now()->lte(Carbon::parse($cantidadHorario->hora_fin)))) {
 
